7b0e568abdcbd88ab88558f512f3cd51dfc65a5d
Bruno Moisão <brunomoisao03@gmail.com>
2024-06-19 22:54:51 +0100

    fix error

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,6 +1,6 @@
 import { AppBar, styled } from "@mui/material";
 
-export const HeaderAppBar = styled(AppBar)(({ theme }) => ({
+export const HeaderAppBar = styled(AppBar)(() => ({
   transition: "background-color 0.8s, color 0.8s",
   backgroundColor: "rgba(0,0,0,0)",
   //boxShadow: "none",
